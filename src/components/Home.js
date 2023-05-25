967ae20dcd3fda2a0086bbc37fc2bfd5807d088c
bethjm <b.jeanne.mills@gmail.com>
2023-05-25 17:15:42 -0400

    added intro area. need to add video

--- src/components/Home.js
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-  return <div>Hi from HOME</div>;
-};
-
-export default Home;
---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import "./Intro.css";
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="content">
+        <h1>Beth Mills</h1>
+        <h3>Full Stack Developer</h3>
+        <p>video goes here</p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
