44648ef9b21a70bd3bc7dbdbcd3d09ef8fed14d6
bethjm <b.jeanne.mills@gmail.com>
2023-05-25 17:44:50 -0400

    styled about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Intro />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import beth from "./images/linkinphoto.jpeg";
+
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="container">
+        <img
+          className="beth-image"
+          src={beth}
+          alt="photo of Beth sitting on a mountain top"
+        />
+        <div className="col-2">
+          <h2>About</h2>
+          <span className="line"></span>
+          <p>
+            As a software engineer with a background in personal training, I
+            bring a unique perspective to complex problem solving. My experience
+            in applying biomechanics to personal training ignited my passion for
+            analytical and empathetic approaches to challenges, which inspired
+            me to pursue software engineering.
+          </p>
+          <p>
+            My commitment to lifelong learning and collaboration has propelled
+            me to constantly seek new ways to innovate and contribute to shared
+            goals. With a dedication to excellence and a passion for knowledge,
+            I am eager to join a team that shares my values and contribute my
+            skills to drive success.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
