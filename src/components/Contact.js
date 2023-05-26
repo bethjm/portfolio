cfc8bdd32dc75f6856563037733f3a0f6171847e
bethjm <b.jeanne.mills@gmail.com>
2023-05-26 11:44:39 -0400

    styled contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,27 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div className="contact" id="contact">
-      contact
+      <div className="container">
+        <h3>Please reach out</h3>
+        <p>Email: bethmills.pn@gmail.com</p>
+
+        <div className="clickable-links">
+          <div>
+            <p>linked in link</p>
+            <img />
+          </div>
+
+          <div>
+            <p>github link</p>
+            <img />
+          </div>
+
+          <div>
+            <p>resume link</p>
+            <img />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
