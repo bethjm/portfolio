8d64832378c977e9b480e193889477df56afe4ee
bethjm <b.jeanne.mills@gmail.com>
2023-05-26 11:51:29 -0400

    added formatting for skills area

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -10,41 +10,122 @@ const Languages = () => {
 
         <h3>Languages</h3>
         <div className="container">
-          <div className="grid-item">1</div>
-          <div className="grid-item">2</div>
-          <div className="grid-item">3</div>
-          <div className="grid-item">4</div>
-          <div className="grid-item">5</div>
-          <div className="grid-item">6</div>
-          <div className="grid-item">7</div>
-          <div className="grid-item">8</div>
-          <div className="grid-item">9</div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
         </div>
 
         <h3>Libraries</h3>
         <div className="container">
-          <div className="grid-item">1</div>
-          <div className="grid-item">2</div>
-          <div className="grid-item">3</div>
-          <div className="grid-item">4</div>
-          <div className="grid-item">5</div>
-          <div className="grid-item">6</div>
-          <div className="grid-item">7</div>
-          <div className="grid-item">8</div>
-          <div className="grid-item">9</div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
         </div>
 
         <h3>Databases</h3>
         <div className="container">
-          <div className="grid-item">1</div>
-          <div className="grid-item">2</div>
-          <div className="grid-item">3</div>
-          <div className="grid-item">4</div>
-          <div className="grid-item">5</div>
-          <div className="grid-item">6</div>
-          <div className="grid-item">7</div>
-          <div className="grid-item">8</div>
-          <div className="grid-item">9</div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
+          <div className="grid-item">
+            <img />
+            <p>NAME OF THING</p>
+          </div>
         </div>
       </div>
     </div>
