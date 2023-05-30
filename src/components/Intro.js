cf0d285cb97609e2503603335336574b3b1df5ce
bethjm <b.jeanne.mills@gmail.com>
2023-05-30 18:00:30 -0400

    reformatted intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,20 +1,18 @@
 import "./Intro.css";
 import React from "react";
 
-// const videosrc = "https://www.youtube.com/watch?v=hMgJNbKhi3E";
-
 const Intro = () => {
   return (
     <div className="intro" id="intro">
       <div className="content">
         <div className="content-1">
           <h1>Beth Mills</h1>
-          <h3>Full Stack Developer</h3>
+          <h3>Full Stack Web Developer</h3>
         </div>
         <div className="content-2">
           <iframe
-            width="560"
-            height="315"
+            width="760"
+            height="515"
             src="https://www.youtube.com/embed/hMgJNbKhi3E"
             title="Youtube Player"
             frameborder="0"
